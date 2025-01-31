0b8cbe3d9b8400261f7eb728e9dd3da4c2e2e801
vlad1111p <vlad1111p@gmail.com>
2025-01-31 18:04:21 +0200

    Change back to ollama embeddings

--- src/main.py
+++ src/main.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     # analyze(java_user_query, java_project_path, java_language)
     python_project_path = "C:/Users/vlad/PycharmProjects/ai-project-reader"
     python_language = "python"
-    python_user_query = "please improve ChromaDBManager"
+    python_user_query = "can you make me README for github of the whole project also mention in the usage that it can be run from main by specifying the language python or java model_type=chatgpt, model_name=gpt-4o or llama and project path "
     # python_user_query = "Refactor me the ai_analyzer.py"
     analyze(python_user_query, python_project_path, python_language, model_type="chatgpt", model_name="gpt-4o")
